b41e950ea2c29cf0e43baa4fa079d75623bac09b
nbratek <bratek@student.agh.edu.pl>
2024-08-29 17:24:07 +0200

    add implementation sending confirmation email

--- src/main/java/pl/nataliabratek/project/domain/service/EmailService.java
+++ src/main/java/pl/nataliabratek/project/domain/service/EmailService.java
@@ -1,23 +1,23 @@
-//package pl.nataliabratek.project.domain.service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class EmailService {
-//    @Autowired
-//    private JavaMailSender mailSender;
-//
-//    public void sendConfirmationEmail(String to, String token){
-//        String subject = "Potwierdzenie rejestracji";
-//        String confirmationUrl = "http://localhost:8080/api/v1/users/confirm/" + token;
-//        String messageText = "Kliknij w link, aby potwierdzic konto";
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(to);
-//        message.setSubject(subject);
-//        message.setText(messageText);
-//        mailSender.send(message);
-//    }
-//}
+package pl.nataliabratek.project.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendConfirmationEmail(String to, String token){
+        String subject = "Potwierdzenie rejestracji";
+        String confirmationUrl = "http://localhost:8080/api/v1/users/confirm/" + token;
+        String messageText = "Kliknij w link, aby potwierdzic konto" + confirmationUrl;
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(messageText);
+        mailSender.send(message);
+    }
+}
